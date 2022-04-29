f11414fbe83235881fb28cd662f4d67c272419c2
jblanda5 <78463169+jblanda5@users.noreply.github.com>
2022-04-29 15:55:13 -0400

    Added tracefiles and ran simulations with their address values

--- project_4/project_4.srcs/sim_1/new/TB_dat_cache.v
+++ project_4/project_4.srcs/sim_1/new/TB_dat_cache.v
@@ -37,8 +37,27 @@ done,
 clk
 );
 integer i;
-reg [31:0]ADDRESSES[31:0];
+reg [31:0]ADDRESSES[59999:0];
 assign address = ADDRESSES[i];
+
+//Read from file, load addresses with values.
+integer data_file;
+integer scan_file;
+integer i;
+initial begin
+    data_file = $fopen("trace1.txt","r");
+    if (data_file == 0) begin
+        $display("data_file handle was NULL");
+        $finish;
+    end
+    for (i=0;i<57432;i=i+1) begin //57432 for trace1, 59855 for trace2
+        ADDRESSES[i] = 0;
+        scan_file = $fscanf(data_file, "%d\n", ADDRESSES[i]);
+    end
+    #1;
+    $fclose(data_file);
+end
+
 initial begin
 clk <= 0;
     while(1) begin
